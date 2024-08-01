e7894a6e398101de116504eba4a52eecb2af4265
yazan <1210145@student.birzeit.edu>
2024-08-01 22:17:10 +0300

    Added files

---
+++ project.sh
@@ -0,0 +1,10 @@
+show_menu() {
+  printf "________Medical Test Management System_________\n\n"
+  echo "1) Add a new medical test"
+  echo "2) Search for a test by patient ID"
+  echo "3) Search for all abnormal tests"
+  echo "4) Get the average test values"
+  echo "5) Update an existing test result"
+}
+
+show_menu
