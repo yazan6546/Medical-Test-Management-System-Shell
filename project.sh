dcf046c533c0eb8906de77c10336714966d57fee
yazan <1210145@student.birzeit.edu>
2024-08-02 10:04:31 +0300

    Made find more concise

--- project.sh
+++ project.sh
@@ -6,6 +6,38 @@ show_menu() {
   echo "3) Search for all abnormal tests"
   echo "4) Get the average test values"
   echo "5) Update an existing test result"
+  echo ""
+}
+
+show_find_menu() {
+  echo "1) Retrieve all patient tests"
+  echo "2) Retrieve all up normal patient tests"
+  echo "3) Retrieve all patient tests in a given specific period"
+  echo "4) Retrieve all patient tests based on test status"
+  echo "5) Go back to menu"
+}
+
+handle_find_case() {
+
+  find_patient_tests $1 # saves all patients with input id in an array "arr"
+  while true; do
+
+  show_find_menu
+  echo ""
+  echo "Choose your option"
+  read option
+
+  case "$option"
+  in
+
+      1) echo "${arr[@]}" | tr " " "\n"
+         echo "";;
+      2) continue ;;
+      3) continue ;;
+      4) continue ;;
+      5) break;;
+    esac
+  done
 }
 
 check_valid_date() {
@@ -33,12 +65,12 @@ check_valid_date() {
 }
 
 
-save_patients() {
-  arr=()
-  while IFS= read -r line; do
-  arr+=("$line")
-  done < "$1"
-}
+#save_patients() {
+#  arr=()
+#  while IFS= read -r line; do
+#  arr+=("$line")
+#  done < "$1"
+#}
 
 #
 # Function that takes patient ID and prints all tests of this patient
@@ -46,37 +78,60 @@ save_patients() {
 
 find_patient_tests() {
 
-  count=0
-  found=()
-  for patient in "${arr[@]}"
-  do
-      id=$(echo "$patient" | cut -d":" -f1)
-      if [ "$id" -eq "$1" ]
-      then
-        echo ${arr[$count]}
-      fi
+#  count=0
+#  found=()
+#  for patient in "${arr[@]}"
+#  do
+#      id=$(echo "$patient" | cut -d":" -f1)
+#      if [ "$id" -eq "$1" ]
+#      then
+#        echo "${arr[$count]}"
+#      fi
+#
+#      count=$((count + 1))
+#  done\
 
-      count=$((count + 1))
-  done
+
+  arr=()
+
+  while IFS= read -r line; do
+    arr+=("$line")
+  done < <(grep "^$1" medicalRecord.txt)
+}
+
+filter_spaces() {
+  cat "$1" | tr -d " " > temp
+  mv temp "$1"
 
 }
 
-find_abnormal_patient_tests() {
 
-  count=0
-  found=()
-  for patient in "${arr[@]}"
-  do
-      id=$(echo "$patient" | cut -d":" -f1)
-      if [ "$id" -eq "$1" ]
-      then
-        echo ${arr[$count]}
-      fi
 
-      count=$((count + 1))
-  done
+filter_spaces medicalRecord.txt
+#find_patient_tests 1300511
+#
+#echo "${arr[@]}" | tr " " "\n"
 
-}
+while true; do
+  show_menu
+  echo "Choose your option."
+  read option
 
-save_patients medicalRecord.txt
-find_patient 1300511
+  case "$option"
+  in
+
+  1) ;;
+  2)
+    echo "enter patient ID"
+    read id
+    handle_find_case $id
+    ;;
+  3);;
+  4);;
+  5);;
+  *) echo "Wrong option!";;
+
+  esac
+done
+
+
